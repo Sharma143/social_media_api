14f0d6d16089cfd0f19a4c4a1fb650602263c20f
Sharma143 <46776124+Sharma143@users.noreply.github.com>
2024-08-21 17:30:07 +0530

    Update index.js

--- socialmediaapi/index.js
+++ socialmediaapi/index.js
@@ -16,11 +16,12 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://sboyapati:Sai@1501@cluster0.cvpfa.mongodb.net/';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => {
+        console.log(`server running in ${PORT}`)
+    })).catch((err) => console.log(err.message));
 
 mongoose.set('useFindAndModify', false);
+
