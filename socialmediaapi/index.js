35ea5fcf7cb1dba2fe5fb1b3eb667b37c9d996dc
Jagadeesh-aedulla <108652185+Jagadeesh-aedulla@users.noreply.github.com>
2024-08-23 00:09:31 +0530

    Add files via upload

--- socialmediaapi/index.js
+++ socialmediaapi/index.js
@@ -1,27 +1,37 @@
-
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import cors from 'cors';
+import AWS from 'aws-sdk';
 
+// Import routes
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
 const app = express();
 
-app.use(express.json({ limit: '30mb', extended: true }))
-app.use(express.urlencoded({ limit: '30mb', extended: true }))
+// Middleware
+app.use(express.json({ limit: '30mb', extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.json("Hellow world")
+})
+// Routes
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
+// Set up DynamoDB
+AWS.config.update({
+  region: 'us-east-1', // or your region
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => {
-        console.log(`server running in ${PORT}`)
-    })).catch((err) => console.log(err.message));
-
-mongoose.set('useFindAndModify', false);
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
